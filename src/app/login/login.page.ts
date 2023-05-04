fb599be5dfd38d6656689a03b3d4924b9f1d1a24
Gonzalo Galdames <shalow.impact@gmail.com>
2023-05-04 00:27:53 -0400

    basepetsos

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+
+export class LoginPage implements OnInit {
+  public loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  public login(): void {
+    // Código para realizar el inicio de sesión
+  }
+  ngOnInit() {
+    
+  }
+
+}
