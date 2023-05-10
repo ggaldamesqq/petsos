bde81f9ca14aceecbee13f8ee542360f966d8e74
Jeremy <80927018+jeremy-droid@users.noreply.github.com>
2023-05-10 00:41:39 -0400

    Mapa-Formulario
    Agregue el mapa, hice un formulario para crear pins

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -1,9 +1,10 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
 
 import { Tab3PageRoutingModule } from './tab3-routing.module';
 
@@ -13,6 +14,7 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
+    ReactiveFormsModule,
     Tab3PageRoutingModule
   ],
   declarations: [Tab3Page]
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  public publicacionForm: FormGroup;
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    
+    this.publicacionForm = this.formBuilder.group({
+      ubicacion: ['', Validators.required],
+      selectedFile: ['', Validators.required],
+      titulo: ['', Validators.required],
+      description: ['', Validators.required],
+      categoria: ['option1'] // Valor por defecto
+    });
+
+  }
+
+
+  public publicar(): void {
+    // Código para crear publicacion
+  }
+
+  ngOnInit(){ 
+  }
 
 }
